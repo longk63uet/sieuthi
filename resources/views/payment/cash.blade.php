f8f54743a72eae39ff88adfad939627b7deffed1
Linh Pham <linhpn2402@gmail.com>
2022-05-21 02:17:48 +0700

    them db

--- resources/views/payment/cash.blade.php
+++ resources/views/payment/cash.blade.php
@@ -20,7 +20,8 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12 text-center mt-5">
-            <h3 class="text-center mb-4" style="font-size: 30px; color: red">Bạn đã đặt hàng thành công</h3>
+            <h3 class="text-center mb-4" style="font-size: 30px; color: red">Bạn đã đặt hàng thành công! </h3>
+            <p style="margin-bottom: 30px; color:black"><b>Chúng tôi sẽ liên hệ ngay với bạn để xác nhận đơn nha!!!</b></p>
         </div>
     </div>
 </div>
